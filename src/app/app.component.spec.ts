34e5773146216e581efa5f9b21d49570839f9049
Gladys-m <gladysmitambo@gmail.com>
2024-02-22 12:35:37 +0400

    Added a header component with the title 'Task Tracker' and added the global style of class container to the nested app-header component in the app.component.html file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-dream-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-app');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
